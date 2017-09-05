d9cad3b58faeddad746fe2140ad68cc8ffa60520
i-makinori <maau3p@gmail.com>
2017-09-05 21:12:19 +0900

    [add+] level6.hs

--- level6.hs
+++ level6.hs
@@ -1,3 +1,10 @@
 -- T:E.\\ = |:S:| # ? V(github.button) (?)
 
 
+nil = "" -- ((=) nil ["") = ../fai/..]
+main = putStrLn nil -- (eval (main lambda^n))
+
+
+{-------------------------
+.eval => | (https://paiza.io/projects/tcRMxiBNgVgBL7YsLGRCRw) |
+-------------------------}
