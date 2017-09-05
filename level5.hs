7d1fa222d2d0f96acb33d3f165fa51e76bf6599c
i-makinori <maau3p@gmail.com>
2017-09-05 23:22:47 +0900

    [+] 5.hs (leeeeeeeeeeeeeeeeeeeeeeeeeeeevel) (-)
    e=>o
    l=>g
    el=>gl

---
+++ level5.hs
@@ -0,0 +1,38 @@
+{------------------------
+
+T=TimTim 1.1 => .blog.V(5.warp(-1)).(../.tag/.title/..) = (|:S:|)
+  # ^-1(?)
+
+T=Tim1.2     => .blog.V(5.warp(-1)).(../.tag/.title/..) = (|:S:|)
+
+-------------------------}
+
+-- ...
+
+data T = Tim Int | TimTim (Int,Int) | TimTimTim(Int, Int, Int) -- ..=|:S:|
+t :: T
+t=Tim 1.1
+
+data R = Rim Int | RimRim (Int,Int) | RimRimRim(Int, Int, Int) -- ..=|:S:|
+r :: R
+r=Rim 1.1
+
+data Q = Qim Int | QimQim (Int,Int) | QimQimQim(Int, Int, Int) -- ..=|:S:|
+q :: Q
+q=Qim 1.1
+
+data I = Iim Int | IimIim (Int,Int) | IimIimIim(Int, Int, Int) -- ..=|:S:|
+i :: I
+i=Iim 1.1
+
+data A = Aim Int | AimAim (Int,Int) | AimAimAim(Int, Int, Int) -- ..=|:S:|
+a :: A
+a=Aim 1.1
+
+data V = Vim Int | VimVim (Int,Int) | VimVimVim(Int, Int, Int) -- ..=|:S:|
+v :: V
+v=Vim 1.1 -- Vim => E:max # lol
+
+-- ...
+
+-- [V(E)]im => Dim -- (!=) .dimension
